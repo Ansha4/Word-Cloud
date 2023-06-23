cbc9c3447815918d7a120edc5a8c36279f924027
Ansha4 <104002999+Ansha4@users.noreply.github.com>
2023-06-23 17:49:49 +0530

    C1M6L2_Final_Project_V3.py

--- C1M6L2_Final_Project_V3.py
+++ C1M6L2_Final_Project_V3.py
@@ -33,16 +33,6 @@ import io
 import sys
 
 
-# Whew! That was a lot. All of the installs and imports for your word cloud script and uploader widget have been completed. 
-# <br><br>
-# **IMPORTANT!** If this was your first time running the above cell containing the installs and imports, you will need save this notebook now. Then under the File menu above,  select Close and Halt. When the notebook has completely shut down, reopen it. This is the only way the necessary changes will take affect.
-# <br><br>
-# To upload your text file, run the following cell that contains all the code for a custom uploader widget. Once you run this cell, a "Browse" button should appear below it. Click this button and navigate the window to locate your saved text file.
-
-# In[19]:
-
-
-# This is the uploader widget
 
 def _upload():
 
@@ -62,13 +52,7 @@ def _upload():
 _upload()
 
 
-# The uploader widget saved the contents of your uploaded file into a string object named *file_contents* that your word cloud script can process. This was a lot of preliminary work, but you are now ready to begin your script. 
 
-# Write a function in the cell below that iterates through the words in *file_contents*, removes punctuation, and counts the frequency of each word.  Oh, and be sure to make it ignore word case, words that do not contain all alphabets and boring words like "and" or "the".  Then use it in the `generate_from_frequencies` function to generate your very own word cloud!
-# <br><br>
-# **Hint:** Try storing the results of your iteration in a dictionary before passing them into wordcloud via the `generate_from_frequencies` function.
-
-# In[22]:
 
 
 file_contents = """I was sitting in my easy-chair, idly turning the pages of a
@@ -100,7 +84,7 @@ def calculate_frequencies(file_contents):
     punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
     uninteresting_words = ["the", "a", "to", "if", "is", "it", "of", "and", "or", "an", "as", "i", "me", "my",     "we", "our", "ours", "you", "your", "yours", "he", "she", "him", "his", "her", "hers", "its", "they", "them",     "their", "what", "which", "who", "whom", "this", "that", "am", "are", "was", "were", "be", "been", "being",     "have", "has", "had", "do", "does", "did", "but", "at", "by", "with", "from", "here", "when", "where", "how",     "all", "any", "both", "each", "few", "more", "some", "such", "no", "nor", "too", "very", "can", "will", "just"]
     
-    # LEARNER CODE START HERE
+
     word_count = {}
     final_text = []
     
@@ -125,17 +109,9 @@ def calculate_frequencies(file_contents):
     return cloud.to_array()
 
 
-# If you have done everything correctly, your word cloud image should appear after running the cell below.  Fingers crossed!
-
-# In[23]:
-
-
-# Display your wordcloud image
 
 myimage = calculate_frequencies(file_contents)
 plt.imshow(myimage, interpolation = 'nearest')
 plt.axis('off')
 plt.show()
 
-
-# If your word cloud image did not appear, go back and rework your `calculate_frequencies` function until you get the desired output.  Definitely check that you passed your frequecy count dictionary into the `generate_from_frequencies` function of `wordcloud`. Once you have correctly displayed your word cloud image, you are all done with this project. Nice work!
